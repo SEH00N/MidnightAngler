a0d558e1dc1e6d64c45a97b1cabe9c255c8fb0f9
SEH00N <06psh0501@gmail.com>
2023-09-12 23:10:03 +0900

    check geometry closed

--- Assets/01. Scripts/System/Fhishing/CatchFish.cs
+++ Assets/01. Scripts/System/Fhishing/CatchFish.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -10,21 +11,65 @@ public class CatchFish : MonoBehaviour
     {
         
     }
-
+    bool debuggg = false;
     private void Update()
     {
         if(Input.GetKeyDown(KeyCode.A))
             Detect(col2d.points.Select(x => (Vector3)x).ToArray());
+
+        if(Input.GetKeyDown(KeyCode.Space))
+            debuggg = !debuggg;
     }
 
     public void Detect(Vector3[] positions)
     {
-        Collider2D[] boundedCols = DetectBoundingArea(positions);
-        List<Collider2D> detectedCols = DetectGeometryArea(boundedCols, positions);
+        Vector3[] geometry = IsGeometryClosed(positions);
+        if(geometry == null)
+            return;
+
+        Debug.Log("Geometry Closed");
+        Collider2D[] boundedCols = DetectBoundingArea(geometry);
+        List<Collider2D> detectedCols = DetectGeometryArea(boundedCols, geometry);
 
         foreach(Collider2D col in detectedCols)
             // if(col.TryGetComponent<FishBehaviour>(out FishBehaviour fish))
                 Debug.Log(col.name);
+    }
+
+    private Vector3[] IsGeometryClosed(Vector3[] positions)
+    {
+        if(positions.Length < 4)
+            return null;
+
+
+        Vector3 lastStartPoint = positions[positions.Length - 2];
+        Vector3 lastEndPoint = positions[positions.Length - 1];
+
+        for(int i = 0; i < positions.Length - 3; i++)
+        {
+            Vector3 startPoint = positions[i];
+            Vector3 endPoint = positions[i + 1];
+
+            if(debuggg)
+            {
+                Debug.DrawLine(startPoint, endPoint, Color.red, Time.deltaTime);
+                Debug.DrawLine(lastStartPoint, lastEndPoint, Color.green, Time.deltaTime);
+                Debug.Break();
+            }
+
+            Vector3 intersection;
+            bool result = GeometryExtension.GetSegmentIntersection(startPoint, endPoint, lastStartPoint, lastEndPoint, out intersection);
+
+            if(result) {
+                ArraySegment<Vector3> geomtry = new ArraySegment<Vector3>(positions, i, positions.Length - i);
+                geomtry.Array[i] = intersection;
+                geomtry.Array[geomtry.Array.Length - 1] = intersection;
+
+                return geomtry.Array;
+            }
+        }
+
+        return null;
     }
 
     private Collider2D[] DetectBoundingArea(Vector3[] positions)
